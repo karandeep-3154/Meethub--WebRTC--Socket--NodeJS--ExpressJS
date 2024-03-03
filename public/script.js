2659754e7a139e2939db99b9f053de872eb2d17b
Broody-Boy <arshdeep2875@gmail.com>
2024-03-03 13:54:55 +0530

    Added Play/Stop video feature

--- public/script.js
+++ public/script.js
@@ -141,3 +141,31 @@ const setUnmuteButton = () => {
   `
   document.querySelector('.main__mute_button').innerHTML = html;
 }
+
+//Play/Stop our video:
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideoButton();
+  } else {
+    setStopVideoButton();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setStopVideoButton = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideoButton = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
