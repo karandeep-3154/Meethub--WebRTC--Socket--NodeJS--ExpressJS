e9c75878d457d0e3da02fa3a8cba8f38a4f31858
Broody-Boy <arshdeep2875@gmail.com>
2024-03-03 13:26:49 +0530

    Added mute unmute feature

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ myVideo.muted = true;
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audio: false,
+    audio: true,
   })
   .then((stream) => {
     myVideoStream = stream;
@@ -112,4 +112,32 @@ Note that broadcast is different from emit: emit sends to all people in room whi
 const scrollToBottom = () => {
   let chatWindow = document.querySelector(".main__chat_window");
   chatWindow.scrollTop = chatWindow.scrollHeight;
+}
+
+//Mute/Unmute our audio
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
 }
